3752a6c120230df987a054756aa461513c08891b
achou <amychou124@gmail.com>
2017-03-08 00:57:05 -0800

    set up SSL context

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import sys
 import argparse
+from OpenSSL import SSL
+
+versions = {"tlsv1.0": SSL.TLSv1_METHOD, "sslv3": SSL.SSLv3_METHOD}
+# "tlsv1.1": SSL.TLSv1_1_METHOD, "tlsv1.2": SSL.TLSv1_2_METHOD don't work, why??
 
 def main(args=None):
-    """The main routine."""
+    parser = argparse.ArgumentParser()
+    initArgParser(parser)
+    args = parser.parse_args()
+    context = SSL.Context(versions[args.version])
 
-    print("This is the main routine.")
-    print("It should do something interesting.")
-
-    # Do argument parsing here (eg. with argparse) and anything else
-    # you want your project to do.
+def initArgParser(parser):
+    parser.add_argument("--tlsv1.0", dest = "version", action="store_const",
+        const="tlsv1.0", default="tlsv1.2",
+        help="Forces scurl to use TLS version 1.0 when connecting to a remote TLS server")
+    parser.add_argument("--tlsv1.1", dest = "version", action="store_const",
+        const="tlsv1.1", default="tlsv1.2",
+        help="Forces scurl to use TLS version 1.1 when connecting to a remote TLS server")
+    parser.add_argument("--tlsv1.2", dest = "version", action="store_const",
+        const="tlsv1.2", default="tlsv1.2",
+        help="Forces scurl to use TLS version 1.2 when connecting to a remote TLS server")
+    parser.add_argument("--sslv3", "-3", dest = "version", action="store_const",
+        const="sslv3", default="tlsv1.2",
+        help="Forces curl to use SSL version 3 when negotiating with a remote SSL server")
