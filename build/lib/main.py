9893686d7e959c75dc49b6eee4e6c002d9404e7d
Sagnik Majumder <Sagnik Majumder>
2017-03-08 17:23:36 -0800

    argument parsing, validating URLs

--- build/lib/main.py
+++ build/lib/main.py
@@ -1,26 +1,30 @@
 import sys
 import argparse
 from OpenSSL import SSL
+from OpenSSL import crypto
+from urlparse import urlparse
 
-versions = {"tlsv1.0": SSL.TLSv1_METHOD, "sslv3": SSL.SSLv3_METHOD}
-# "tlsv1.1": SSL.TLSv1_1_METHOD, "tlsv1.2": SSL.TLSv1_2_METHOD don't work, why??
+versions = {'tlsv1.0': SSL.TLSv1_METHOD, 'tlsv1.1': SSL.TLSv1_1_METHOD, 'tlsv1.2': SSL.TLSv1_2_METHOD, 'sslv3' : SSL.SSLv3_METHOD}
 
 def main(args=None):
     parser = argparse.ArgumentParser()
     initArgParser(parser)
     args = parser.parse_args()
+
+    print(args)
+
     context = SSL.Context(versions[args.version])
 
 def initArgParser(parser):
-    parser.add_argument("--tlsv1.0", dest = "version", action="store_const",
-        const="tlsv1.0", default="tlsv1.2",
-        help="Forces scurl to use TLS version 1.0 when connecting to a remote TLS server")
-    parser.add_argument("--tlsv1.1", dest = "version", action="store_const",
-        const="tlsv1.1", default="tlsv1.2",
-        help="Forces scurl to use TLS version 1.1 when connecting to a remote TLS server")
-    parser.add_argument("--tlsv1.2", dest = "version", action="store_const",
-        const="tlsv1.2", default="tlsv1.2",
-        help="Forces scurl to use TLS version 1.2 when connecting to a remote TLS server")
-    parser.add_argument("--sslv3", "-3", dest = "version", action="store_const",
-        const="sslv3", default="tlsv1.2",
-        help="Forces curl to use SSL version 3 when negotiating with a remote SSL server")
+    parser.add_argument('--tlsv1.0', action = 'store_const', const = 'tlsv1.0', dest = 'version', default = 'tlsv1.2')
+    parser.add_argument('--tlsv1.1', action = 'store_const', const = 'tlsv1.1', dest = 'version', default = 'tlsv1.2')
+    parser.add_argument('--tlsv1.2', action = 'store_const', const = 'tlsv1.2', dest = 'version', default = 'tlsv1.2')
+    parser.add_argument('--sslv3', '-3', action = 'store_const', const = 'sslv3', dest = 'version', default = 'tlsv1.2')
+
+    parser.add_argument('--ciphers', action = 'store', dest = 'ciphers')
+    parser.add_argument('--crlfile', action = 'store', dest = 'crlfile')
+    parser.add_argument('--cacert', action = 'store', dest = 'cacert')
+    parser.add_argument('--allow-stale-certs', action = 'store', dest = 'exp_days')
+    parser.add_argument('--pinnedcertificate', action = 'store', dest = 'cert_file')
+
+    parser.add_argument('url', nargs = '*')
